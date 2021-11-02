9d560e7c9889644a938c947fbf6a96bf4b07cdce
madhanse29 <srkmadhan@gmail.com>
2021-11-02 14:58:31 +0530

    mano

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   const names=[
  {name: "ajith", pic:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIbfDzNPtnPQF6u02N9c4z9QvRUPlIFGu91A&usqp=CAU"},
  {name: "kumar",pic:"http://www.ga.berkeley.edu/wp-content/uploads/2015/08/146-1468479_my-profile-icon-blank-profile-picture-circle-hd.png"},
- {name: "mapp", pic:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBpFo-yE9V9f6idya5UCXlwvuTf229Z2Sing&usqp=CAU"},
+ {name: "mano", pic:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBpFo-yE9V9f6idya5UCXlwvuTf229Z2Sing&usqp=CAU"},
  {name: "madhan",pic:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIbfDzNPtnPQF6u02N9c4z9QvRUPlIFGu91A&usqp=CAU"},]
 
   return (
